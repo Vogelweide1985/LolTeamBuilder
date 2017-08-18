701c28a857cbf475a57f7ce4b357dde268bd7604
Vogelweide1985 <Vogelweide1985@hotmail.de>
2017-08-18 10:08:02 +0200

    added stats functionality

--- js/work.js
+++ js/work.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 		   /*var appendEl = $("<div class='row'></div>").appendTo("#lol_icons"); */ /*
 		} 
 		*/
-
+		/* Show Square Images */
 		$("<div class='col-xs-3 col-md-2 square_png'>").appendTo("#lol_icons");
 		var image = $('<img/>');
 		image.attr('class', 'img-responsive square');
@@ -27,14 +27,14 @@ $(document).ready(function() {
 });
 
 
-n_champs = 0;
+var n_champs = 0;
 function clickSquare() {
 	if (n_champs < 5) {
 	
 	$(this).closest('div').addClass('filter_out') 
-	champ_id = +$(this).data('champ_id');
+	var champ_id = +$(this).data('champ_id');
 	var image = $('<img/>');
-	image.attr('class', 'img-responsive');
+	image.attr('class', 'img-responsive chosen_champions');
 	image.attr('alt', stats[champ_id].image_png );
 	image.attr('src', 'img/riot/full/' + stats[champ_id].image_png);
 	image.attr('data-champ_id', champ_id);
@@ -62,3 +62,39 @@ function clickImage() {
 	n_champs--;
 	$(this).closest('div').remove();
 };
+
+
+$(document).ready(function() {
+	$('body').on('click', function(event){
+	getStats(1);
+	getStats(2);
+	getStats(3);
+	getStats(4);
+	getStats(5);
+	totalStat();
+	});
+});
+
+function getStats(param) {
+		var	champ_imgs = $('.chosen_champions');
+		var total = 0;
+		if (champ_imgs.length > 0) {
+			$(champ_imgs).each(function() {
+			    total += parseFloat($(this).attr('data-value_' + param), 10) || 0; 
+			});
+			total = Math.round(total / $(champ_imgs).length);
+		};
+		$('#erg' + param).html(total + "%");
+};
+
+function totalStat() {
+	var champ_stats = $('.results');
+	var total = 0;
+		if (champ_stats.length > 0) {
+			$(champ_stats).each(function() {
+			    total += parseFloat($(this).html().replace("%", ""), 10) || 0; 
+			});
+			total = Math.round(total / $(champ_stats).length);
+		};
+		$('#overall').html(total + "%");
+}
