a5c9c028e91c2b4bafa0abda2a7edbf69fa62b3b
vogelweide1985 <vogelweide1985@hotmail.de>
2017-08-20 11:30:16 +0200

    added Disclaimer and ga disable script

---
+++ js/ga.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+var gaProperty = 'UA-XXXXXXX-X';
+var disableStr = 'ga-disable-' + gaProperty;
+if (document.cookie.indexOf(disableStr + '=true') > -1) {
+window[disableStr] = true;
+}
+function gaOptout() {
+document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; 
+path=/';
+window[disableStr] = true;
+}
+</script> 
